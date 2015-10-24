de9d418afadac998981cf80397c00d42a99c88fb
MartaF <martafisher@hotmail.com>
2015-10-23 23:37:33 -0700

    added the 2 functions & descriptions to cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file is saved in ProgrammingAssignment2 in Git
+## create a matrix object and then cache its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The first function creates a matrix
+makeCacheMatrix <- function(x = matrix()) {    
+  mtx <- NULL                   #set value of the matrix to NULL
+  set <- function(y){          
+    x <<- y
+    mtx <<- NULL          #define a function to reset the matrix
+  }
+  get <- function() x           #define a function to retrieve the matrix
+  setinverse <- function(inverse) mtx <<- inverse   #define a function to reset the inverse matrix
+  getinverse <- function() mtx  #define a function to retrieve the inverse matrix
+  list(set = set, get = get,
+       setinverse = setinverse, 
+       getinverse = getinverse) #initialize a list that allows you to access all the functions
 }
 
 
-## Write a short comment describing this function
+## the second function uses the solve function to create the inverse matrix 
+## and caches that matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){              
+  inv <- x$getinverse()               #define the inverse
+  if(!is.null(inv)){                  #if the inverse already has a value
+    message("getting cached data")   #tell the user we are using the cached matrix,
+    return(inv)                 #return that cached matrix, and exit the function
+  }
+  data <- x$get()            #get the matrix from the makeCacheMatrix function
+  inv <- solve(data, ...)    #calculate the matrix inverse
+  x$setinverse(inv)          #sets the inverse matrix using the command from makeCacheMatrix
+  inv                        #print the inverse matrix
 }
